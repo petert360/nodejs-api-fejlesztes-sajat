bc96e8fdfc8016923ca826d9b185fdc22ccb16df
petert360 <peter.t360@hotmail.com>
2021-07-18 20:11:48 +0200

    Mongoose - One to Many - Populate 1

--- src/models/person.model.js
+++ src/models/person.model.js
@@ -3,8 +3,20 @@ const mongoose = require('mongoose');
 // timestamps: https://mongoosejs.com/docs/guide.html#timestamps
 // legyen minden dokumentumnak egy createdAt és updatedAt  mezője
 const PersonSchema = mongoose.Schema(
-    { firstName: String, lastName: String, email: String },
-    { timestamps: true }
+    {
+        firstName: String,
+        lastName: String,
+        email: String,
+        posts: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Post',
+            },
+        ],
+    },
+    {
+        timestamps: true,
+    }
 );
 
 module.exports = mongoose.model('Person', PersonSchema);
